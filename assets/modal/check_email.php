b4972a16de8f04d8907a2eb42320cae857326ea0
FDC-FrancisJude <fdc.francisjude@gmail.com>
2024-06-24 14:29:09 +0800

    Update check_email.php

--- assets/modal/check_email.php
+++ assets/modal/check_email.php
@@ -3,32 +3,32 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     include_once "../../database/conn.php";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo json_encode(['emailExists' => false]);
-    // $requestData = json_decode(file_get_contents('php://input'), true);
-    // $email = $requestData['email'];
+    // $conn = new mysqli($servername, $username, $password, $dbname);
+    // if ($conn->connect_error) {
+    //     die("Connection failed: " . $conn->connect_error);
+    // }
+
+    $requestData = json_decode(file_get_contents('php://input'), true);
+    $email = $requestData['email'];
       
-    //  $query = "SELECT email FROM student_acc WHERE email = ?";
-    //  $stmt = $conn->prepare($query);
-    //  $stmt->bind_param("s", $email);
-    //  $stmt->execute();
-    //  $result = $stmt->get_result();
+     $query = "SELECT email FROM student_acc WHERE email = ?";
+     $stmt = $conn->prepare($query);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $result = $stmt->get_result();
      
-    //  if ($result->num_rows > 0) {
-    //     $row = $result->fetch_assoc();
-    //      $existing_email = $row['email'];
+     if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+         $existing_email = $row['email'];
          
-    //      if ($existing_email == $email) {
-    //         echo json_encode(['emailExists' => true]);
-    //     } else {
-    //         echo json_encode(['emailExists' => false]);
-    //     }
-    //  } else {
-    //     echo json_encode(['emailExists' => false]);
-    //  }
+         if ($existing_email == $email) {
+            echo json_encode(['emailExists' => true]);
+        } else {
+            echo json_encode(['emailExists' => false]);
+        }
+     } else {
+        echo json_encode(['emailExists' => false]);
+     }
     }
 ?> 
 
