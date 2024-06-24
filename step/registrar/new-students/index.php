3564442fbcbf4872d20b1ee59dbf2b84f264fb3d
FDC-FrancisJude <fdc.francisjude@gmail.com>
2024-06-24 09:56:19 +0800

    Update index.php

--- step/registrar/new-students/index.php
+++ step/registrar/new-students/index.php
@@ -641,29 +641,29 @@ require '../../../database/regis3.php';
     <script src="../../assets/js/script.js"></script>
 
     <script>
-        // $(document).ready(function() {
-        //     $('.search-box input[type="text"]').on("keyup input", function() {
-        //         /* Get input value on change */
-        //         var inputVal = $(this).val();
-        //         var resultDropdown = $(this).siblings(".result");
-        //         if (inputVal.length) {
-        //             $.get("backend-search.php", {
-        //                 term: inputVal
-        //             }).done(function(data) {
-        //                 // Display the returned data in browser
-        //                 resultDropdown.html(data);
-        //             });
-        //         } else {
-        //             resultDropdown.empty();
-        //         }
-        //     });
+        $(document).ready(function() {
+            $('.search-box input[type="text"]').on("keyup input", function() {
+                /* Get input value on change */
+                var inputVal = $(this).val();
+                var resultDropdown = $(this).siblings(".result");
+                if (inputVal.length) {
+                    $.get("backend-search.php", {
+                        term: inputVal
+                    }).done(function(data) {
+                        // Display the returned data in browser
+                        resultDropdown.html(data);
+                    });
+                } else {
+                    resultDropdown.empty();
+                }
+            });
 
-        //     // Set search input value on click of result item
-        //     $(document).on("click", ".result p", function() {
-        //         $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
-        //         $(this).parent(".result").empty();
-        //     });
-        // });
+            // Set search input value on click of result item
+            $(document).on("click", ".result p", function() {
+                $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+                $(this).parent(".result").empty();
+            });
+        });
     </script>
     <!-- end: JS -->
 
