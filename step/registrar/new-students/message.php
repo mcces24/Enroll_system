71881189b05843b5b4c5d524a0c5ca5f094d76ce
FDC-FrancisJude <fdc.francisjude@gmail.com>
2024-06-24 10:09:18 +0800

    revert

--- step/registrar/new-students/message.php
+++ step/registrar/new-students/message.php
@@ -1,14 +1,14 @@
 <?php
 if (isset($_SESSION['message']) && $_SESSION['message'] != '') {
 ?>
-  <!-- <script>
+  <script>
     swal({
       title: "<?php echo $_SESSION['message'] ?>",
       text: "Have a nice day!",
       icon: "<?php echo $_SESSION['message_icon'] ?>",
       button: "OKAY",
     });
-  </script> -->
+  </script>
 <?php
   unset($_SESSION['message']);
 }
