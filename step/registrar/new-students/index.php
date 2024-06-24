4dd028b49c958ae69452004a546def570efbbb65
FDC-FrancisJude <fdc.francisjude@gmail.com>
2024-06-24 09:58:18 +0800

    Update index.php

--- step/registrar/new-students/index.php
+++ step/registrar/new-students/index.php
@@ -219,11 +219,14 @@ require '../../../database/regis3.php';
     <!-- start: Icons -->
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <!-- start: Icons -->
+    <script src="script.js"></script>
     <!-- start: CSS -->
     <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../assets/css/style.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <!-- end: CSS -->
-    <title>Test New Students | Registrar Office - Madridejos Community College</title>
+    <title>New Students | Registrar Office - Madridejos Community College</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.css">
 
 
@@ -260,10 +263,10 @@ require '../../../database/regis3.php';
 </head>
 
 <body style="width: 100%;">
-    <div class="loader-wrapper" id="preloader">
+    <!-- <div class="loader-wrapper" id="preloader">
         <span class="loader"><span class="loader-inner"></span></span>
     </div>
-    <!-- <link rel="stylesheet" type="text/css" href="../../../loader/styles.css" />
+    <link rel="stylesheet" type="text/css" href="../../../loader/styles.css" />
     <script>
         var loader = document.getElementById("preloader");
         window.addEventListener("load", function() {
@@ -271,8 +274,8 @@ require '../../../database/regis3.php';
         })
     </script> -->
 
-    <?php //include('message.php'); ?>
-    <?php //include('edit_delete_modal.php'); ?>
+    <?php include('message.php'); ?>
+    <?php include('edit_delete_modal.php'); ?>
 
     <!-- start: Sidebar -->
     <?php include '../inc/navbar.php'  ?>
