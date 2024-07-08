d95ef7fe5708de1c13d1762f2017d8528519368d
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-07-08 13:23:48 +0800

    Update config2.php

--- database/config2.php
+++ database/config2.php
@@ -1,8 +1,8 @@
 <?php 
 	$host = 'localhost';
-$username = 'root';
-$pass = '';
-$db = 'mcc_es';
+$username = 'u510162695_mcces';
+$pass = 'MccAdmin1';
+$db = 'u510162695_mcc_es';
 
 $db = new mysqli($host,$username,$pass,$db);
 
