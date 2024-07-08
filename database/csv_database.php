09dee1f1b3cabcb42f18ce8b8a1a5eb2084912b8
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-07-08 13:25:25 +0800

    Update csv_database.php

--- database/csv_database.php
+++ database/csv_database.php
@@ -1,6 +1,6 @@
 <?php 
 	$host="localhost"; // Host name.
-$db_user="root"; //mysql user
-$db_password=""; //mysql pass
-$db='mcc_es'; // Database name.
+$db_user="u510162695_mcces"; //mysql user
+$db_password="MccAdmin1"; //mysql pass
+$db='u510162695_mcc_es'; // Database name.
  ?>
