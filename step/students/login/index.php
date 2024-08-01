3526397f98cbb4483ce0ff878f2e880cde747b41
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-08-01 12:27:20 +0800

    ss

--- step/students/login/index.php
+++ step/students/login/index.php
@@ -95,7 +95,7 @@ Copy code
                         <input type="email" class="form-control" id="forgotEmail" name="email" placeholder="Email" required>
                     </div>
                     <div class="alert alert-danger" id="forgotPasswordAlert" style="display: none;"></div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn">Submit</button>
                 </form>
             </div>
         </div>
