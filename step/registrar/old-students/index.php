8a7b52116ac074c0d6f24a56c0597a9c937a6592
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-08-06 19:57:10 +0800

    ss

--- step/registrar/old-students/index.php
+++ step/registrar/old-students/index.php
@@ -178,7 +178,7 @@ else{
     <?php include('edit_delete_modal.php'); ?>
 
     <!-- start: Sidebar -->
-    <?php include '../inc/navbar.php'  ?>
+    <?php include '../inc/dropdown.php'  ?>
     <div class="sidebar-overlay"></div>
     <!-- end: Sidebar -->
 
