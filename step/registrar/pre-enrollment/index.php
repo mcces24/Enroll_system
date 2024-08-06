20d044b88984887bff6825c4d0c0e257fc01aa1b
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-08-06 19:43:47 +0800

    ss

--- step/registrar/pre-enrollment/index.php
+++ step/registrar/pre-enrollment/index.php
@@ -15,7 +15,7 @@ else{
                                 
         require '../../../database/config.php';
 
-        $querys1 = "SELECT * FROM students WHERE status_type = 'Old Students' GROUP BY applicant_id DESC LIMIT 1";
+        $querys1 = "SELECT * FROM students WHERE status_type = 'Enroll' GROUP BY applicant_id DESC LIMIT 1";
         $querys_run1 = mysqli_query ($conn, $querys1);
 
         if (mysqli_num_rows($querys_run1)>0) {
@@ -34,8 +34,7 @@ else{
                                 
         require '../../../database/config.php';
 
-        // $querys1 = "SELECT * FROM students WHERE status_type = 'Pre Old Students' GROUP BY id_number DESC LIMIT 1";
-        $querys1 = "SELECT * FROM students WHERE status_type = 'Old Students' GROUP BY id_number DESC LIMIT 1";
+        $querys1 = "SELECT * FROM students WHERE status_type = 'Pre Old Students' GROUP BY id_number DESC LIMIT 1";
         $querys_run1 = mysqli_query ($conn, $querys1);
 
         if (mysqli_num_rows($querys_run1)>0) {
