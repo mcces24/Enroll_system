8d8f4f7ec18e1447c0d302243072dfe197a08117
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-09-25 19:33:44 +0800

    Update index.php

--- enroll-now/index.php
+++ enroll-now/index.php
@@ -9361,38 +9361,7 @@ $academic = !empty($academicYear) ? "$start-$end" : null;
 
          })
       }
-
-      // Check if geolocation is available
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition, showError);
-} else {
-  console.log("Geolocation is not supported by this browser.");
-}
-
-function showPosition(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-}
-
-function showError(error) {
-  switch(error.code) {
-    case error.PERMISSION_DENIED:
-      console.log("User denied the request for Geolocation.");
-      break;
-    case error.POSITION_UNAVAILABLE:
-      console.log("Location information is unavailable.");
-      break;
-    case error.TIMEOUT:
-      console.log("The request to get user location timed out.");
-      break;
-    case error.UNKNOWN_ERROR:
-      console.log("An unknown error occurred.");
-      break;
-  }
-}
-
-
+   
       function preEnrol(form) {
          const regex = /<script.*?>.*?<\/script>/i;
          for (let field of form.elements) {
