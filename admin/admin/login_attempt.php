df8cbb4adeb6ce18447ba7e8b4ea1da0fbce4738
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-07 16:19:04 +0800

    Update login_attempt.php

--- admin/admin/login_attempt.php
+++ admin/admin/login_attempt.php
@@ -80,12 +80,12 @@ $successful_result_student = $conn->query($successful_sql_student);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attempt Log Admin</title>
+    <title>Attempt Log</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
-    <h2>Attempt Log Admin</h2>
+    <h2>Attempt Log</h2>
 
     <!-- Tabs -->
     <ul class="nav nav-tabs" id="attemptLogsTabs" role="tablist">
