6016e4b84d8356f9b45941a758f8d5ab3762a78d
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-07 14:15:51 +0800

    Update login_attempt.php

--- admin/admin/login_attempt.php
+++ admin/admin/login_attempt.php
@@ -29,7 +29,7 @@ $successful_result_guidance = $conn->query($successful_sql_guidance);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attemp Log Admin</title>
+    <title>Attemp Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
