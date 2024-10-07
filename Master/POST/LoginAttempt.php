c6fb5fb9f591b270fb104531f76a837340721516
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-07 14:31:52 +0800

    Create LoginAttempt.php

---
+++ Master/POST/LoginAttempt.php
@@ -0,0 +1,11 @@
+<?php
+function logLoginAttempt($conn, $email, $type, $location, $completeAddress, $lat, $lon) {
+    date_default_timezone_set('America/New_York');
+    $currentDateTime = date('Y-m-d H:i:s'); // Format: YYYY-MM-DD HH:MM:SS
+    $stmt = $conn->prepare("INSERT INTO login_logs (attemp, portal, type, location, com_location, lat, lon, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $portal = 'admin';
+    $stmt->bind_param("sssssdds", $email, $portal, $type, $location, $completeAddress, $lat, $lon, $currentDateTime);
+    $stmt->execute();
+    $stmt->close();
+}
+?>
