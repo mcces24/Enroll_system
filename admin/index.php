62c43d10fd8eaa4cd6535e0d48a5a4c673b91557
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-07 13:47:54 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -11,6 +11,7 @@ $msg = "";
 
 // Function to log login attempts
 function logLoginAttempt($conn, $email, $type, $location, $completeAddress, $lat, $lon) {
+    date_default_timezone_set('America/New_York');
     $currentDateTime = date('Y-m-d H:i:s'); // Format: YYYY-MM-DD HH:MM:SS
     $stmt = $conn->prepare("INSERT INTO login_logs (attemp, portal, type, location, com_location, lat, lon, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
     $portal = 'admin';
