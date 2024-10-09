c574f943d7338992a56f746a5dbb288f583bcd31
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-09 16:18:40 +0800

    Update NewUserController.php

--- Master/Controller/NewUserController.php
+++ Master/Controller/NewUserController.php
@@ -187,8 +187,6 @@ class NewUserController extends NewUser {
     }
 
     public function forgetPassword($data) {
-
-        print_r($data);
         $responseData = [];
         try {
             // Get data by email
