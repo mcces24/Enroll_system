c7eb918e1afc1d411f11e6dfa57b9ab747a52114
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-10 10:25:37 +0800

    Update index.php

--- step/guidance/new-applicant/index.php
+++ step/guidance/new-applicant/index.php
@@ -143,83 +143,80 @@ $newApplicatTableData = isset($data['newApplicantData']) ? $data['newApplicantDa
                 }
                 console.log(data);
                 if (data.length > 0) {
-
+                    
                     data.forEach((value, index) => {
-                        console.log(value);
-                        // If you need the index as well, you can use it here
-                        // console.log(index); // Uncomment to log the index
-                    });
-                    // $.ajax({
-                    //     dataType: 'json',
-                    //     url: BASE_PATH + '/Master/POST/POST.php',
-                    //     method: "POST",
-                    //     data: {
-                    //         type: 'acceptNewApplicant',
-                    //         data: data
-                    //     },
-                    //     beforeSend: function() {
-                    //         $('#' + id).html('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Accepting...');
-                    //         $('#' + id).addClass('btn-info');
-                    //         $('#' + id).prop('disabled', true);
-                    //         $.each(data, function(key, value) {
-                    //             var id = value.id
-                    //             $('#id-' + id).html('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Accepting...');
-                    //             $('#id-' + id).addClass('btn-info');
-                    //             $('#id-' + id).prop('disabled', true);
-                    //         });
-                    //     },
-                    //     success: function(data) {
-                    //         $.each(data, function(key, value) {
-                    //             var response = JSON.parse(value);
-                    //             var id = response.id;
-                    //             var message = response.message;
-                    //             var type = response.type;
-                    //             var status = response.status;
-                    //             if (status == 'success') {
-                    //                 $('#id-' + id).text(message);
-                    //                 $('#id-' + id).removeClass('btn-danger');
-                    //                 $('#id-' + id).removeClass('btn-info');
-                    //                 $('#id-' + id).addClass('btn-' + type);
-                    //                 $('#table-row-' + id).fadeOut(2000, function() {
-                    //                     $(this).html('');
-                    //                 });
-                    //             } else if (status == 'failed') {
-                    //                 $('#id-' + id).text(message);
-                    //                 $('#id-' + id).removeClass('btn-danger');
-                    //                 $('#id-' + id).removeClass('btn-info');
-                    //                 $('#id-' + id).removeClass('btn-success');
-                    //                 $('#id-' + id).addClass('btn-' + type);
-                    //             } else {
-                    //                 $('#id-' + id).text('Cannot Accept Request');
-                    //                 $('#id-' + id).removeClass('btn-danger');
-                    //                 $('#id-' + id).removeClass('btn-info');
-                    //                 $('#id-' + id).addClass('btn-info');
-                    //             }
-                    //             $('#id-' + id).attr('disabled', false);
-                    //         });
+                        var id = value.id
+                        $('#id-' + id).html('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Accepting...');
+                        $('#id-' + id).addClass('btn-info');
+                        $('#id-' + id).prop('disabled', true);
+                        // $.ajax({
+                        //     dataType: 'json',
+                        //     url: BASE_PATH + '/Master/POST/POST.php',
+                        //     method: "POST",
+                        //     data: {
+                        //         type: 'acceptNewApplicant',
+                        //         data: value
+                        //     },
+                        //     beforeSend: function() {
+                        //         var id = value.id
+                        //         $('#id-' + id).html('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Accepting...');
+                        //         $('#id-' + id).addClass('btn-info');
+                        //         $('#id-' + id).prop('disabled', true);
+                        //     },
+                        //     success: function(data) {
+                        //         $.each(data, function(key, value) {
+                        //             var response = JSON.parse(value);
+                        //             var id = response.id;
+                        //             var message = response.message;
+                        //             var type = response.type;
+                        //             var status = response.status;
+                        //             if (status == 'success') {
+                        //                 $('#id-' + id).text(message);
+                        //                 $('#id-' + id).removeClass('btn-danger');
+                        //                 $('#id-' + id).removeClass('btn-info');
+                        //                 $('#id-' + id).addClass('btn-' + type);
+                        //                 $('#table-row-' + id).fadeOut(2000, function() {
+                        //                     $(this).html('');
+                        //                 });
+                        //             } else if (status == 'failed') {
+                        //                 $('#id-' + id).text(message);
+                        //                 $('#id-' + id).removeClass('btn-danger');
+                        //                 $('#id-' + id).removeClass('btn-info');
+                        //                 $('#id-' + id).removeClass('btn-success');
+                        //                 $('#id-' + id).addClass('btn-' + type);
+                        //             } else {
+                        //                 $('#id-' + id).text('Cannot Accept Request');
+                        //                 $('#id-' + id).removeClass('btn-danger');
+                        //                 $('#id-' + id).removeClass('btn-info');
+                        //                 $('#id-' + id).addClass('btn-info');
+                        //             }
+                        //             $('#id-' + id).attr('disabled', false);
+                        //         });
 
-                    //         if (action != 'single') {
-                    //             $('#' + id).text('Selected Accepted');
-                    //             $('#' + id).addClass('btn-success');
-                    //             $('#' + id).removeClass('btn-info');
-                    //             setTimeout(function() {
-                    //                 $('#' + id).text('Accept Selected');
-                    //                 $('#' + id).addClass('btn-info');
-                    //                 $('#' + id).removeClass('btn-success');
-                    //                 $('#' + id).prop('disabled', false);
-                    //             }, 2000);
-                    //         }
-                    //     }, 
-                    //     error: function(xhr, status, error) {
-                    //         console.error("Error: " + error);
-                    //         console.error("Status: " + status);
-                    //         console.error("Response: " + xhr.responseText);
-                    //         $('#' + id).html("Cannot Accept Request");
-                    //         $('#' + id).removeClass('btn-info');
-                    //         $('#' + id).addClass('btn-danger');
-                    //         $('#' + id).prop('disabled', false);
-                    //     }
-                    // });
+                        //         if (action != 'single') {
+                        //             $('#' + id).text('Selected Accepted');
+                        //             $('#' + id).addClass('btn-success');
+                        //             $('#' + id).removeClass('btn-info');
+                        //             setTimeout(function() {
+                        //                 $('#' + id).text('Accept Selected');
+                        //                 $('#' + id).addClass('btn-info');
+                        //                 $('#' + id).removeClass('btn-success');
+                        //                 $('#' + id).prop('disabled', false);
+                        //             }, 2000);
+                        //         }
+                        //     }, 
+                        //     error: function(xhr, status, error) {
+                        //         console.error("Error: " + error);
+                        //         console.error("Status: " + status);
+                        //         console.error("Response: " + xhr.responseText);
+                        //         $('#' + id).html("Cannot Accept Request");
+                        //         $('#' + id).removeClass('btn-info');
+                        //         $('#' + id).addClass('btn-danger');
+                        //         $('#' + id).prop('disabled', false);
+                        //     }
+                        // });
+                    });
+                    
                     data = [];
                 } else {
                     alert("Please Select atleast one checkbox");
