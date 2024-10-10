33e79a7bd413800e586c712784c9b0781eb10e39
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-10 10:50:04 +0800

    Update index.php

--- step/guidance/accepted-applicant/index.php
+++ step/guidance/accepted-applicant/index.php
@@ -160,73 +160,73 @@ $acceptedApplicantData = isset($data['acceptedApplicantData']) ? $data['accepted
                     console.log("Data more than 1");
                     data.forEach((value, index) => {
                         var id = value.id
+                        console.log(value);
+                        // $.ajax({
+                        //     dataType: 'json',
+                        //     url: BASE_PATH + '/Master/POST/POST.php',
+                        //     method: "POST",
+                        //     data: {
+                        //         type: 'sendGuidanceForm',
+                        //         data: value
+                        //     },
+                        //     beforeSend: function() {
+                        //         $('#' + id).html('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Sending...');
+                        //         $('#' + id).addClass('btn-info');
+                        //         $('#' + id).prop('disabled', true);
+                        //     },
+                        //     success: function(data) {
 
-                        $.ajax({
-                            dataType: 'json',
-                            url: BASE_PATH + '/Master/POST/POST.php',
-                            method: "POST",
-                            data: {
-                                type: 'sendGuidanceForm',
-                                data: value
-                            },
-                            beforeSend: function() {
-                                $('#' + id).html('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Sending...');
-                                $('#' + id).addClass('btn-info');
-                                $('#' + id).prop('disabled', true);
-                            },
-                            success: function(data) {
+                        //         var response = JSON.parse(data);
+                        //         console.log(response);
+                        //         var id = response.id
+                        //         var message = response.message;
+                        //         var type = response.type;
+                        //         var status = response.status;
+                        //         if (status == 'success') {
+                        //             $('#id-' + id).text(message);
+                        //             $('#id-' + id).removeClass('btn-danger');
+                        //             $('#id-' + id).removeClass('btn-info');
+                        //             $('#id-' + id).addClass('btn-' + type);
+                        //             $('#table-row-' + id).fadeOut(2000, function() {
+                        //                 $(this).html('');
+                        //             });
+                        //         } else if (status == 'failed') {
+                        //             $('#id-' + id).text(message);
+                        //             $('#id-' + id).removeClass('btn-danger');
+                        //             $('#id-' + id).removeClass('btn-info');
+                        //             $('#id-' + id).removeClass('btn-success');
+                        //             $('#id-' + id).addClass('btn-' + type);
+                        //         } else {
+                        //             $('#id-' + id).text('Cannot Send Email');
+                        //             $('#id-' + id).removeClass('btn-danger');
+                        //             $('#id-' + id).removeClass('btn-info');
+                        //             $('#id-' + id).addClass('btn-info');
+                        //         }
+                        //         $('#id-' + id).attr('disabled', false);
 
-                                var response = JSON.parse(data);
-                                console.log(response);
-                                var id = response.id
-                                var message = response.message;
-                                var type = response.type;
-                                var status = response.status;
-                                if (status == 'success') {
-                                    $('#id-' + id).text(message);
-                                    $('#id-' + id).removeClass('btn-danger');
-                                    $('#id-' + id).removeClass('btn-info');
-                                    $('#id-' + id).addClass('btn-' + type);
-                                    $('#table-row-' + id).fadeOut(2000, function() {
-                                        $(this).html('');
-                                    });
-                                } else if (status == 'failed') {
-                                    $('#id-' + id).text(message);
-                                    $('#id-' + id).removeClass('btn-danger');
-                                    $('#id-' + id).removeClass('btn-info');
-                                    $('#id-' + id).removeClass('btn-success');
-                                    $('#id-' + id).addClass('btn-' + type);
-                                } else {
-                                    $('#id-' + id).text('Cannot Send Email');
-                                    $('#id-' + id).removeClass('btn-danger');
-                                    $('#id-' + id).removeClass('btn-info');
-                                    $('#id-' + id).addClass('btn-info');
-                                }
-                                $('#id-' + id).attr('disabled', false);
-
-                                if (action != 'single') {
-                                    $('#' + id).text('Selected Sent');
-                                    $('#' + id).addClass('btn-success');
-                                    $('#' + id).removeClass('btn-info');
-                                    $('#' + id).removeClass('btn-danger');
-                                    setTimeout(function() {
-                                        $('#' + id).text('Send Selected');
-                                        $('#' + id).addClass('btn-info');
-                                        $('#' + id).removeClass('btn-success');
-                                        $('#' + id).prop('disabled', false);
-                                    }, 2000);
-                                }
-                            },
-                            error: function(xhr, status, error) {
-                                console.error("Error: " + error);
-                                console.error("Status: " + status);
-                                console.error("Response: " + xhr.responseText);
-                                $('#' + id).html("Cannot Send Email");
-                                $('#' + id).removeClass('btn-info');
-                                $('#' + id).addClass('btn-danger');
-                                $('#' + id).prop('disabled', false);
-                            }
-                        });
+                        //         if (action != 'single') {
+                        //             $('#' + id).text('Selected Sent');
+                        //             $('#' + id).addClass('btn-success');
+                        //             $('#' + id).removeClass('btn-info');
+                        //             $('#' + id).removeClass('btn-danger');
+                        //             setTimeout(function() {
+                        //                 $('#' + id).text('Send Selected');
+                        //                 $('#' + id).addClass('btn-info');
+                        //                 $('#' + id).removeClass('btn-success');
+                        //                 $('#' + id).prop('disabled', false);
+                        //             }, 2000);
+                        //         }
+                        //     },
+                        //     error: function(xhr, status, error) {
+                        //         console.error("Error: " + error);
+                        //         console.error("Status: " + status);
+                        //         console.error("Response: " + xhr.responseText);
+                        //         $('#' + id).html("Cannot Send Email");
+                        //         $('#' + id).removeClass('btn-info');
+                        //         $('#' + id).addClass('btn-danger');
+                        //         $('#' + id).prop('disabled', false);
+                        //     }
+                        // });
                     });
 
                 } else {
