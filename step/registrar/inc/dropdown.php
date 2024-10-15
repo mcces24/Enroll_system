f10ee5bec5bc3c90fcac118f9ce12615ff631ee9
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-15 13:19:34 +0800

    Update dropdown.php

--- step/registrar/inc/dropdown.php
+++ step/registrar/inc/dropdown.php
@@ -8,7 +8,7 @@ $query_new = mysqli_query($conn,$sql_new);
 $new= mysqli_fetch_assoc($query_new);
 $new_students = $new['new_students'] ?? 0;
 
-$sql_new="SELECT students.applicant_id as app_id from students INNER JOIN year_lvl y On students.year_id=y.year_id INNER JOIN course c ON students.course_id=c.course_id WHERE semester_id = '$semester' AND academic = '$academic' AND  status_type = 'Enroll' ORDER BY id DESC LIMIT 1";
+$sql_new="SELECT students.applicant_id as app_id from students INNER JOIN que ON students.id = que.student_id INNER JOIN year_lvl y On students.year_id=y.year_id INNER JOIN course c ON students.course_id=c.course_id WHERE semester_id = '$semester' AND academic = '$academic' AND  status_type = 'Enroll' ORDER BY id DESC LIMIT 1";
 $query_new = mysqli_query($conn,$sql_new);
 $new= mysqli_fetch_assoc($query_new);
 $applicant_id_new = $new['app_id'] ?? 'NONE';
