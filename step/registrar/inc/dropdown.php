1d51646fd70ec754ed1fd257b75f6d4e31814584
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-10-15 13:11:11 +0800

    Update dropdown.php

--- step/registrar/inc/dropdown.php
+++ step/registrar/inc/dropdown.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Manila');
-$currentDate = date('Y-m-d');
+$currentDate = date('Y/m/d');
 echo $currentDate;
 
 $sql_new="SELECT COUNT(*) as new_students from students  INNER JOIN year_lvl y On students.year_id=y.year_id INNER JOIN course c ON students.course_id=c.course_id WHERE semester_id = '$semester' AND academic = '$academic' AND  status_type = 'Enroll'";
